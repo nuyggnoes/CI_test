0465e260d465965f8476326ccb5b35ec4cca111a
임성균 <97024187+nuyggnoes@users.noreply.github.com>
2025-05-17 14:38:09 +0900

    [FEAT] 로그인 기능(수정)

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,6 +1,6 @@
 describe("Login flow", () => {
     it("logs in and redirects to home", () => {
-        cy.visit("http://localhost:3000/clientLogin");
+        cy.visit("http://localhost:3000/test/login");
 
         cy.get('input[name="email"]').type("test@test.com");
         cy.get('input[name="password"]').type("1234");
