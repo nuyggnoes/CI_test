d01749ee2f72af482155c5dba53b9dc9e196203e
임성균 <97024187+nuyggnoes@users.noreply.github.com>
2025-05-17 15:12:41 +0900

    [FEAT] 로그인 기능 수정(최정)

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -19,6 +19,7 @@ const eslintConfig = [
         rules: {
             "@typescript-eslint/no-unused-vars": "off",
             "@typescript-eslint/no-explicit-any": "warn",
+            "@typescript-eslint/no-require-imports": "off",
         },
     },
 ];
